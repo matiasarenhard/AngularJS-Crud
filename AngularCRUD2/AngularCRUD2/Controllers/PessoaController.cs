6d15af9d75c32133115f82a53d5cdd945d243eca
matiasarenhard <matias_arenhard@hotmail.com>
2017-03-28 11:53:50 -0300

    asdasd
    asda

---
+++ AngularCRUD2/AngularCRUD2/Controllers/PessoaController.cs
@@ -0,0 +1,122 @@
+﻿using AngularCRUD2.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Linq;
+
+namespace AngularCRUD2.Areas.Controller
+{
+    public class PessoaController : ApiController
+    {
+
+
+        #region Buscar
+
+        [HttpGet]
+        public object BuscarPessoa()
+        {
+            using (BancoDadosEntities Obj = new BancoDadosEntities())
+            {
+                List<Pessoa> ListaPessoa = Obj.Pessoa.ToList();
+                return ListaPessoa;
+            }
+        }
+        #endregion
+
+
+        #region Gravar
+        [HttpPost]
+        public string GravarPessoa(Pessoa Pessoa)
+        {
+            #region validações
+            if (Pessoa.Email == "" || Pessoa.Email == null)
+                return "Por favor informe o email";
+            if (Pessoa.Telefone == "" || Pessoa.Telefone == null)
+                return "Por favor informe o telefone";
+            if (Pessoa.Nome == "" || Pessoa.Nome == null)
+                return "Por favor informe o nome";
+            if (Pessoa.Idade == 0 || Pessoa.Idade == null)
+                return "Por favor informe a idade";
+            #endregion
+
+            if (Pessoa != null)
+            {
+                using (BancoDadosEntities Obj = new BancoDadosEntities())
+                {
+                    Obj.Pessoa.Add(Pessoa);
+                    Obj.SaveChanges();
+                    return "Pessoa adicionada com sucesso";
+                }
+            }
+            else
+            {
+                return "Não foi possivel cadastrar está pessoa";
+            }
+        }
+        #endregion
+
+        #region Alterar
+        [HttpPost]
+        public string AlterarPessoa(Pessoa Pessoa)
+        {
+            #region validações
+            if (Pessoa.Email == "" || Pessoa.Email == null)
+                return "Por favor informe o email";
+            if (Pessoa.Telefone == "" || Pessoa.Telefone == null)
+                return "Por favor informe o telefone";
+            if (Pessoa.Nome == "" || Pessoa.Nome == null)
+                return "Por favor informe o nome";
+            if (Pessoa.Idade == 0 || Pessoa.Idade == null)
+                return "Por favor informe a idade";
+            #endregion
+
+            if (Pessoa != null)
+            {
+                using (BancoDadosEntities Con = new BancoDadosEntities())
+                {
+                    var pessoa = Con.Entry(Pessoa);
+                    Pessoa Obj = Con.Pessoa.Where(x => x.IdPessoa == Pessoa.IdPessoa).FirstOrDefault();
+                    if (Obj == null)
+                        return "Está Pessoa não foi encontrada em nosso banco de dados, por favor contate o suporte.";
+
+                    Obj.Email = Pessoa.Email;
+                    Obj.Telefone = Pessoa.Telefone;
+                    Obj.Nome = Pessoa.Nome;
+                    Obj.Idade = Pessoa.Idade;
+                    Con.SaveChanges();
+                    return "Pessoa atualizada com sucesso!";
+                }
+            }
+            else
+            {
+                return "Pessoa não atualizada";
+            }
+        }
+        #endregion
+
+        #region Deletar
+        [HttpPost]
+        public string DeletePessoa(Pessoa Pessoa)
+        {
+            if (Pessoa != null)
+            {
+                using (BancoDadosEntities Con = new BancoDadosEntities())
+                {
+                    var pessoa = Con.Entry(Pessoa);
+                    if (pessoa.State == System.Data.Entity.EntityState.Detached)
+                    {
+                        Con.Pessoa.Attach(Pessoa);
+                        Con.Pessoa.Remove(Pessoa);
+                    }
+                    Con.SaveChanges();
+                    return "Pessoa deletada com sucesso";
+                }
+            }
+            else
+            {
+                return "Pessoa não deletada";
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ AngularCRUD2/AngularCRUD2/Scripts/AngularCode.js
@@ -0,0 +1,98 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope, $http) {
+ 
+    $scope.GravarPessoa = function () {
+        var Action = $("#btnSave").attr("type");
+        if (Action == "submit") {
+            $scope.Pessoa = {};
+            $scope.Pessoa.Nome = $scope.F_Nome;
+            $scope.Pessoa.Email = $scope.F_Email;
+            $scope.Pessoa.Telefone = $scope.F_Telefone;
+            $scope.Pessoa.Idade = $scope.F_Idade;
+            $scope.Pessoa.IdPessoa = null;
+            $http({
+                method: "POST",
+                url: "/Pessoa/GravarPessoa",
+                datatype: "json",
+                data:  JSON.stringify({
+                        nome: $scope.F_Nome,
+                        email: $scope.F_Email,
+                        telefone: $scope.F_Telefone,
+                        idade: $scope.F_Idade
+                        })
+            }).then(function (response) {
+                $('#myModal').modal('toggle');
+                alert(response.data);
+                $scope.BuscarPessoa();
+                $scope.F_Email = "";
+                $scope.F_Nome = "";
+                $scope.F_Telefone = "";
+                $scope.F_Idade = "";
+            })
+        } else {
+            $scope.Pessoa = {};
+            $scope.Pessoa.Nome = $scope.F_Nome;
+            $scope.Pessoa.Email = $scope.F_Email;
+            $scope.Pessoa.Telefone = $scope.F_Telefone;
+            $scope.Pessoa.Idade = $scope.F_Idade;
+            $scope.Pessoa.IdPessoa = $scope.F_IdPessoa;
+            $http({
+                method: "post",
+                url: "/Pessoa/AlterarPessoa",
+                datatype: "json",
+                data: JSON.stringify($scope.Pessoa)
+            }).then(function (response) {
+                $('#myModal').modal('toggle');
+                alert(response.data);
+                $scope.BuscarPessoa();
+                $scope.F_Email = "";
+                $scope.F_Nome = "";
+                $scope.F_Telefone = "";
+                $scope.F_Idade = "";
+                document.getElementById("btnSave").setAttribute("value", "Submit");
+                document.getElementById("btnSave").style.backgroundColor = "cornflowerblue";
+            })
+        }
+    }
+
+
+  // Buscar
+    $scope.BuscarPessoa = function () {
+        $http({
+            method: "get",
+            url: "/Pessoa/BuscarPessoa"
+        }).then(function (response) {
+            $scope.pessoas = response.data;
+        }, function () {
+            alert("Error");
+        })
+    };
+
+
+    //Deletar
+    $scope.DeletePessoa = function (Obj) {
+        $http({
+            method: "post",
+            url: "/Pessoa/DeletePessoa",
+            datatype: "json",
+            data: JSON.stringify(Obj)
+        }).then(function (response) {
+            alert(response.data);
+            $scope.BuscarPessoa();
+        })
+    };
+
+
+
+    //Update
+    $scope.UpdatePessoa = function (Obj) {
+        $('#myModal').modal('show');
+        $scope.F_Nome = Obj.Nome;
+        $scope.F_Email = Obj.Email;
+        $scope.F_Telefone = Obj.Telefone;
+        $scope.F_Idade = Obj.Idade;
+        $scope.F_IdPessoa = Obj.IdPessoa;
+        $("#btnSave").attr("type","Update");
+        document.getElementById("btnSave").style.backgroundColor = "Yellow";
+    }
+})
